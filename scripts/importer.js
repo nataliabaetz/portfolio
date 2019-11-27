ec7e599f4efffdd0afcf5abb4129c42f1c4d53bf
Natalia <nataliabaetz@gmail.com>
2019-11-27 09:28:43 -0800

    More styling

---
+++ scripts/importer.js
@@ -0,0 +1,44 @@
+$(function(){ $("header").load("header.html") });
+$(function(){ $("footer").load("footer.html") });
+$(function(){ 
+  const caseStudies = $("li.caseStudy")
+  if(caseStudies.length > 0){
+    $.get("caseStudy.html", function( loaded ) {
+      caseStudies.each((index, li) => {
+        template = $(loaded)
+        template.prop('id', $(li).prop('id'))
+        template.find('h1').text($(li).data('name'))
+        template.find('a').prop('href', $(li).data('link'))
+        $(li).replaceWith(template)
+      })
+    });
+  }
+});
+$(function(){ 
+  const sectionHeaders = $("section.sectionHeader")
+  if(sectionHeaders.length > 0){
+    $.get("sectionHeader.html", function( loaded ) {
+      sectionHeaders.each((index, section) => {
+        template = $(loaded)
+        template.find('h2').text($(section).data('title'))
+        template.find('h6').text($(section).data('subtitle'))
+        template.find('p').text($(section).data('description'))
+        $(section).replaceWith(template)
+      })
+    });
+  }
+});
+
+$(function(){ 
+  const sectionHeaders = $("div.subHeader")
+  if(sectionHeaders.length > 0){
+    $.get("subHeader.html", function( loaded ) {
+      sectionHeaders.each((index, section) => {
+        template = $(loaded)
+        template.find('h3').text($(section).data('title'))
+        template.find('p').text($(section).data('description'))
+        $(section).replaceWith(template)
+      })
+    });
+  }
+});
